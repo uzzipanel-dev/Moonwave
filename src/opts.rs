d7c62615210cfc212b832962389d4a1a7dd9cccf
uzzi2k_ <118691276+uzzipanel-dev@users.noreply.github.com>
2026-09-07 12:41:56 +0300

    Update backend and login URLs to new IP address

--- src/opts.rs
+++ src/opts.rs
@@ -1,6 +1,6 @@
-pub const BACKEND_URL: &str = "http://192.168.1.69:8080"; // Set valid IP, not 127.0.0.1 for mobile
+pub const BACKEND_URL: &str = "http://91.200.220.198:8080"; // Set valid IP, not 127.0.0.1 for mobile
 
 // Partyhub shit
 pub const USE_PARTYHUB: bool = false;
-pub const LOGIN_URL: &str = "http://192.168.1.69:5000/login";
-pub const REGISTER_URL: &str = "http://192.168.1.69:5000/register";
+pub const LOGIN_URL: &str = "http://91.200.220.198:8080/login";
+pub const REGISTER_URL: &str = "http://91.200.220.198:8080/register";

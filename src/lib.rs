050dd144a03045dcf64374cd6638b7e897a3466d
uzzi2k_ <118691276+uzzipanel-dev@users.noreply.github.com>
2026-09-07 15:06:26 +0300

    Refactor moonwave_entry to use main with ctor

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,21 @@
 #![allow(unexpected_cfgs)]
+use ctor::ctor;
 mod protocol;
 mod opts;
 mod wkwebview;
 mod cfnetwork;
 
-// Punct de intrare standard exportat pe care loader-ul îl poate apela în siguranță
-#[no_mangle]
-pub extern "C" fn moonwave_entry() {
-    unsafe {
-        protocol::init_moonwave_url_protocol();
-    }
+// CLEAN CODE WOW
+#[ctor]
+fn main() {
+    unsafe {
+        protocol::init_moonwave_url_protocol();
+    }
 
-    if opts::USE_PARTYHUB {
-        unsafe {
-            wkwebview::init_moonwave_webview_delegate();
-            cfnetwork::init_moonwave_cfnetwork_hook();
-        }
-    }
+    if opts::USE_PARTYHUB {
+        unsafe {
+            wkwebview::init_moonwave_webview_delegate();
+            cfnetwork::init_moonwave_cfnetwork_hook();
+        }
+    }
 }
